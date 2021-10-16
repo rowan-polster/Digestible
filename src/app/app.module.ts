d3dedfdbed4c5329eba9c1cc349d1dee51b23e4b
Lesley_Heyer <lesley@wewhistle.com>
2021-10-16 14:23:41 -0500

    Work on Carousel started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatCarouselModule } from '@ngmodule/material-carousel';
+
 import { AppComponent } from './app.component';
 import { FormEntryCardComponent } from './form-entry-card/form-entry-card.component';
 import { DragDropCardComponent } from './drag-drop-card/drag-drop-card.component';
@@ -10,6 +12,7 @@ import { ImageCarouselComponent } from './image-carousel/image-carousel.componen
 import { RecipeImageComponent } from './image-carousel/recipe-image/recipe-image.component';
 import { RecipeModalComponent } from './recipe-modal/recipe-modal.component';
 import { IngredientTileComponent } from './ingredient-tile/ingredient-tile.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,9 @@ import { IngredientTileComponent } from './ingredient-tile/ingredient-tile.compo
     IngredientTileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatCarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/image-carousel/image-carousel.component.ts
+++ src/app/image-carousel/image-carousel.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageCarouselComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+
+    
   }
 
 }
