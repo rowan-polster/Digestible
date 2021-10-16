90495dd608952d24275a8964aa05510fd07371df
Torin <torinsamuelson@yahoo.com>
2021-10-16 10:34:25 -0500

    Implement Recipe class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '@structures/Recipe';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Digestible';
+
+    recipes : Recipe[];
+
+    constructor() {
+        this.recipes = [];
+    }
+
+    ngOnInit() {
+
+        const apiUrl = "https://digestible-test-server.herokuapp.com/5";
+        const fetchConfig: object = {
+            method: 'GET',
+            mode: 'cors'
+        };
+        fetch(apiUrl, fetchConfig).then(response => response.json().then(json => {
+
+            for (const recipePayload of json.hits) {
+
+                const ingredients = [];
+                for (const ingredient of recipePayload.recipe.ingredients) {
+                    ingredients.push(ingredient.food);
+                }
+
+                this.recipes.push(
+                    new Recipe({
+                        label: recipePayload.recipe.label,
+                        image : recipePayload.recipe.image,
+                        link : recipePayload.recipe.url,
+                        ingredients: ingredients
+                    })
+                );
+                
+            }
+
+        }))
+
+    }
+    
 }
