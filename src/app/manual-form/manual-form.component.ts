33d6d4bf1dbc996be251ea885a2fdb45eee85670
Rowan Polster <rowandownriver94@gmail.com>
2021-10-16 15:14:39 -0500

    Add manual-form form

--- src/app/manual-form/manual-form.component.ts
+++ src/app/manual-form/manual-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-manual-form',
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManualFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() width: string;
+  @Input() height: string;
+
+  constructor() { 
+    this.width = '';
+    this.height = '';
+  }
 
   ngOnInit(): void {
   }
