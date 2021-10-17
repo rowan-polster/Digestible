6e6f8012a2d8e7ad9a2a4b17902f43f6c70c62bb
Torin <torinsamuelson@yahoo.com>
2021-10-17 11:23:24 -0500

    Add variable length shelf lifes

--- src/app/structures/ingredient.ts
+++ src/app/structures/ingredient.ts
@@ -1,3 +1,5 @@
+import { toBase64String } from "@angular/compiler/src/output/source_map";
+
 export class Ingredient {
 
     name: string;
@@ -7,7 +9,26 @@ export class Ingredient {
     constructor(name: string, category: string) {
         this.name = name;
         this.category = category;
-        this.originalShelfLife = 5;
+        switch (category.toLowerCase()) {
+            case "produce":
+                this.originalShelfLife = 5
+                break;
+            
+            case "dairy":
+                this.originalShelfLife = 7;
+                break;
+
+            case "meat":
+                this.originalShelfLife = 4;
+                break;
+
+            case "other":
+                this.originalShelfLife = 10;
+                break;
+            
+            default:
+                this.originalShelfLife = 8;
+        }
     }
     
 }
